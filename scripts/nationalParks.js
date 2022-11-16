f270dca529ea5c5a2a30bead05679ab7e609c47f
niyaridout55@gmail.com <Niya Ridout>
2022-11-16 11:46:53 -0500

    scripts/nationalParks.js

--- scripts/nationalParks.js
+++ scripts/nationalParks.js
@@ -19,24 +19,20 @@ document.getElementById('selectLocation').addEventListener("change", function ()
     console.log(selectedOptions);
     let parksFiltered = nationalParksArray.filter(function (nationalParksArray) {
         if (nationalParksArray.State === selectedOptions) {
-
-        }
-        return selectedOptions === nationalParksArray.State;
-
+        } return selectedOptions === nationalParksArray.State;
     }); console.log(parksFiltered)
     
     let parkList = document.getElementById('parkList');
+    while (parkList.hasChildNodes()) {
+        parkList.removeChild(parkList.firstChild);
+    }
     for (let i = 0; i < parksFiltered.length; i++) {
-         liTag = document.createElement('li');
-        liTag.innerText = "  Name: " + parksFiltered[i].LocationName + "  Address: " + parksFiltered[i].Address + "  City:  " + parksFiltered[i].City + " Zip: " + parksFiltered[i].ZipCode + " Phone:  " + parksFiltered[i].Phone + "  " + "  " + "  " + "  " + " " + " ";
+        liTag = document.createElement('li');
+        liTag.innerText = " " + "  Name: " + parksFiltered[i].LocationName + "  Address: " + parksFiltered[i].Address + "  City:  " + parksFiltered[i].City + " Zip: " + parksFiltered[i].ZipCode + " Phone:  " + parksFiltered[i].Phone + "  " + "  " + "  " + "  " + " " + " ";
         parkList.appendChild(liTag);
         const ulElement = document.querySelector('.parkOutput');
         ulElement.style.backgroundColor = 'green';
         document.getElementById('parkList').style.color = "aliceblue";
-        // if (liTag) {
-        //     selectedOptions[i] =
-        // }
-       
     } 
 
 });
